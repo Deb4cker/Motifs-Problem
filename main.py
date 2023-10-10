8ab336c9c9eaeff38088c6fdb369433b6e9a4ca8
Nicolas D. <78985734+Deb4cker@users.noreply.github.com>
2023-10-09 23:16:24 -0300

    creating nodes and edges

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 from pyomo.environ import *
 import random
+import classes
 
-class node: #vertice tem um id e uma cor
+numeros = classes.numeros
+
+class node: #vertice tem um id (numero) e uma cor (string)
     def __init__(self, id, color):
-        self.id: id
-        self.color: color
+        self.id = id
+        self.color = color
     def __str__(self):
         return f"[{self.id} {self.color}]"
 
-class edge: #aresta tem um vertice de origem e um de destino
+class edge: #aresta tem um vertice de origem (u) e um de destino (v
     def __init__(self, u, v):
-        self.u: u
-        self.v: v
+        self.u = u
+        self.v = v
     def __str__(self):
         return f"{self.u} -> {self.v}"
+
+class graph: #grafo tem um conjunto de vertices e um conjunto de arestas
+    def __init__(self, nodes, edges):
+        self.nodes = nodes
+        self.edges = edges
+    def __str__(self):
+        return f"{self.nodes} {self.edges}"
+
 
 colors = ['red',  'blue',   'green',
         'yellow', 'orange', 'purple',
@@ -26,9 +37,21 @@ def create_nodes(n): #cria n vertices com cores aleatorias
         nodes.append(node(i ,colors[random.randint(0, len(colors)-1)]))
     return nodes
 
-nodes = create_nodes(12)
-print(nodes)
+nodes = create_nodes(12) #cria 12 vertices
 
+for n in nodes:
+    print(n)
+
+def create_edges(n): #cria arestas aleatorias
+    edges = []
+    for i in range(n):
+        edges.append(edge(nodes[random.randint(0, len(nodes)-1)], nodes[random.randint(0, len(nodes)-1)]))
+    return edges   
+
+edges = create_edges(12) #cria 12 arestas
+
+for e in edges:
+    print(e)
 #model:
 
 # model = ConcreteModel()

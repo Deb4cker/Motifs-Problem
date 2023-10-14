2a4fa544739b958e812c03293b6f62ff86a63549
Nicolas D. <78985734+Deb4cker@users.noreply.github.com>
2023-10-14 19:06:38 -0300

    Adding a file to store the instances; explanation of each one of them.

---
+++ instances.py
@@ -0,0 +1,12 @@
+# 1- numero TOTAL de vertices do grafo;
+
+# 2- numero de cores;
+
+# 3- numero de vertices com cores repetidas;
+
+# 4- tamanho do motif, soma de todas as vezes que uma cor aparece no motif:
+    #exemplo: o motif tem 1 amarelo, 2 vermelhos e 1 azul, logo o tamanho do motif é 4;
+
+# 5 - quantas cores tem no motif.
+#----------------------------------------------------------------------------------------#
+
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import random
 #---------------------classes---------------------#
 
 class node: #vertice tem um id (numero) e uma cor (string)
+    edges = []
     def __init__(self, id, color):
         self.id = id
         self.color = color
@@ -13,7 +14,9 @@ class node: #vertice tem um id (numero) e uma cor (string)
 class edge: #aresta tem um vertice de origem (u) e um de destino (v
     def __init__(self, u, v):
         self.u = u
+        u.edges.append(self)
         self.v = v
+        v.edges.append(self)
     def __str__(self):
         return f"{self.u} <----> {self.v}"
 
@@ -43,23 +46,24 @@ def create_edges(n): #cria arestas aleatorias
         edges.append(edge(nodes[random.randint(0, len(nodes)-1)], nodes[random.randint(0, len(nodes)-1)]))
     return edges   
 
+def print_graph(graph):
+    print("vertices:")
+    for n in graph.nodes:
+        print(n)
+    print("\ngrafo:")
+    for e in graph.edges:
+        print(e)
+
 #---------------------main----------------------#
 
 nodes = create_nodes(12) #cria 12 vertices
 edges = create_edges(12) #cria 12 arestas
 theGraph = graph(nodes, edges) #cria o grafo
 
-print("vertices:")
-for n in theGraph.nodes:
-    print(n)
-
-print("\ngrafo:")
-for e in theGraph.edges:
-    print(e)
-
+print_graph(theGraph) #printa o grafo
 #---------------------pyomo----------------------#
     
 #model:
 
 # model = ConcreteModel()
-# model.obj = Objective(expr = 1+y, sense=minimize)
+# model.obj = Objective(expr = 1, sense=minimize)

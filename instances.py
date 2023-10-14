ce4499707108ba688f9975a0cbf7c9e7bd1605e5
Nicolas D. <78985734+Deb4cker@users.noreply.github.com>
2023-10-14 19:33:12 -0300

    instances

--- instances.py
+++ instances.py
@@ -1,8 +1,22 @@
+import random
+#----------------------------------------------------------------------------------------#
+
+
+
 # 1- numero TOTAL de vertices do grafo;
+graph_lenght = 12 #add a graph lenght here;
 
 # 2- numero de cores;
+def generate_colors(total):
+    colors = []
+    while len(colors) < total:
+        color = tuple(random.sample(range(256), 3))
+        if color not in colors:
+            colors.append(color)
+    return colors
 
 # 3- numero de vertices com cores repetidas;
+
 
 # 4- tamanho do motif, soma de todas as vezes que uma cor aparece no motif:
     #exemplo: o motif tem 1 amarelo, 2 vermelhos e 1 azul, logo o tamanho do motif é 4;

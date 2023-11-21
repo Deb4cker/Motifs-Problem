64162b59bd1b65b324a2772198899c72c7b5e4be
Nicolas Debacher <ndebacher@gmail.com>
2023-11-21 19:51:24 -0300

    restart part_2

--- instances.py
+++ instances.py
@@ -1,10 +1,5 @@
 import random
 
-class Motif:
-    def __init__(self, subgraph):
-        self.subgraph = subgraph
-        self.notConnectedEdges = len(subgraph) * (len(subgraph) - 1) // 2 - len(subgraph)
-
 def generateVertex(nvertex):
     vertex = []
     for i in range(nvertex):
@@ -49,65 +44,22 @@ def generateVc(cores, ncolors):
                 Vc[c].append(i)
     return Vc
 
-def generateSolution(vertex):
-    solution = [0 for _ in range(len(vertex))]
-    return solution
-
-def countEdges(vertex, edges):
-    solution = [0 for _ in range(len(vertex))]
-    for i in range(len(edges)):
-        solution[edges[i][0]] += 1
-        solution[edges[i][1]] += 1
-    return solution
-
-def getConnectedVertices(vertex, edges, v):
-    connectedVertices = []
-    for i in range(len(edges)):
-        if edges[i][0] == v:
-            connectedVertices.append(edges[i][1])
-        elif edges[i][1] == v:
-            connectedVertices.append(edges[i][0])
-    return connectedVertices
-
-def getColorsOfVertices(vertex, vertexColor):
-    colors = {}
-    for v in vertex:
+def generateVerticesWithColors(vertexColor):
+        verticesWithColors = {}
         for color, vertices in vertexColor.items():
-            if v in vertices:
-                colors[v] = color
-                break
-    return colors
+            for vertex in vertices:
+                verticesWithColors[vertex] = color
+        return verticesWithColors
 
-def getVerticeColor(vertice, vertexColor):
-    for color, vertex in vertexColor.items():
-        if vertice in vertex:
-            return color
+def generateGraph(vertex, edges):
+    graph = {}
+    for v in vertex:
+        graph[v] = []
+    for edge in edges:
+        v1, v2 = edge
+        graph[v1].append(v2)
+        graph[v2].append(v1)
+    return graph
 
-def countColorByM(vertice, color, M, countArray, possibleSolution):
-    if M[color] > countArray[color]:
-        countArray[color] = countArray[color] + 1
-        possibleSolution.append(vertice)
-
-
-def verticeVisited(v, visitedVerticesArray):
-    visitedVerticesArray[v - 1] = True
-
-
-def getRandomNotVisitedVertice(visitedVerticesArray):
-    notVisitedVertex = [i for i, visited in enumerate(visitedVerticesArray) if not visited]
-    return random.choice(notVisitedVertex)
-
-def solve(Vertex, Edges, Vc, randomVertice, M):
-    countArray = [0 for _ in range(len(M))]               # Array de contagem de cores. Conta quantas vezes uma cor foi usada 
-    visitedVertices = [False for _ in range(len(Vertex))] # Array de vértices visitados
-    possibleSolution = []                                 # Array de solução possível
-    
-    countColorByM(randomVertice, getVerticeColor(randomVertice, Vc), M, countArray) #vai contabilizar a cor do vertice aleatorio
-
-    while M != countArray:                                                  # Enquanto o array de contagem de cores for diferente do array de cores
-        connectedVertices = getConnectedVertices(Edges, randomVertice)      # Pega os vértices conectados ao vértice aleatório
-        for v in connectedVertices:                                         # Para cada vértice conectado
-            vColor = getVerticeColor(v, Vc)                                 # Pega a cor do vértice
-            countColorByM(v, vColor, M, countArray, possibleSolution)       # Contabiliza a cor do vértice (se for possível)
-        verticeVisited(v, visitedVertices)                                  # Marca o vértice como visitado (nesta iteração)
-        randomVertice = getRandomNotVisitedVertice(Vertex, visitedVertices) # Pega um vértice aleatório não visitado
+def getVerticeColor(vertex, dicitonary):
+    return dicitonary[vertex]
--- main.py
+++ main.py
@@ -13,35 +13,14 @@ E = [(6, 9), (5, 7), (6, 0), (6, 8), (6, 1), (3, 4), (4, 9)] # Conjunto de arest
 Vc = {0: [2, 4, 0, 9, 6], 1: [3, 5], 2: [1, 8, 7]} # Conjunto de vértices e suas cores
 M = [2, 1, 1] #O subgrafo deve ter a quantidade tal para cada cor
 
+colorDictionary = generateVerticesWithColors(Vc)
+graph = generateGraph(V, E)
+
 # V = generateVertex(nvertex)  # Conjunto de vértices
 # C = generateColors(ncolors)  # Conjunto de cores
 # E = generateEdges(V)  # Conjunto de arestas
 # cores = generateVerticesColors(V, ncolors)
 # M = generateM(ncolors, cores)
 # Vc = generateVc(cores, ncolors)
-solucoes = [2, 1, 5, 6, 3]
-solution = generateSolution(V) # Solução inicial
-edgeCountList = countEdges(V, E) # Lista de contagem de arestas
-randomvertice = 6 # Vértice aleatório  
-connectedvertices = getConnectedVertices(V, E, randomvertice)
-colorsOfConnectedVertices = getColorsOfVertices(connectedvertices, Vc)
-print(M)
-print(E)
-print(Vc)
-print(solution)
-print(edgeCountList)
-print(randomvertice)
-print(connectedvertices)
-print(colorsOfConnectedVertices)
 
-vertex_colors = getColorsOfVertices(V, Vc)
-motifs = find_motifs(V, E, Vc, M)
 
-#choose aleatory vertex
-#iterate in each connected vertex until have tha pattern [2 of color 0, 1 of color 1 and 1 of color 2]
-#while this, mount the motif
-#insert the motif in a solution array
-#search for the next vertex
-#repeat the process until not have more vertex to search
-#get the motif with the min count in the array of solutions
-#print this motif as the solution
